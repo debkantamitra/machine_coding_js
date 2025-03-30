e2bf11e48f6eb1c2b98a1c1df46aeb48314c9741
Debkanta Mitra <debkantamitra52@gmail.com>
2025-03-30 15:49:03 +0530

    comment

--- problems/nested_comments/script.js
+++ problems/nested_comments/script.js
@@ -1,3 +1,6 @@
+// TODO: fix duplication of attaching event listener instead check each time if input text is not undefined
+// TODO: use a simpler way of implementation
+
 (function () {
   const handleComment = () => {
     const commentInputs = document.querySelectorAll(".comment__input");
@@ -13,6 +16,7 @@
       commentInput.addEventListener("submit", (e) => {
         e.preventDefault();
         const text = commentInput.querySelector("input").value;
+        // inside each level of the document.. we can use query selector to grab the children, as we do with document!!
 
         if (text) {
           const newCommentContainer = document.createElement("div");
