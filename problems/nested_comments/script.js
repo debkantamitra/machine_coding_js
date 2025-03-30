b912f6288e13fdab9f4bf1d0df85655193b29ec2
Debkanta Mitra <debkantamitra52@gmail.com>
2025-03-30 15:33:49 +0530

    wip: nested comment with bugs

--- problems/nested_comments/script.js
+++ problems/nested_comments/script.js
@@ -1,15 +1,48 @@
 (function () {
-  const commentInput = document.querySelector(".comment__input");
-  const commentReplyBtn = document.querySelector("#comment__reply__btn");
+  const handleComment = () => {
+    const commentInputs = document.querySelectorAll(".comment__input");
+    const commentReplyBtns = document.querySelectorAll(".comment__reply__btn");
 
-  commentInput.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const comment = document.querySelector("#comment");
+    commentReplyBtns.forEach((btn, index) => {
+      btn.addEventListener("click", () => {
+        commentInputs[index].classList.add("comment__input-active");
+      });
+    });
 
-    console.log(comment.value);
-  });
+    commentInputs.forEach((commentInput) => {
+      commentInput.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const text = commentInput.querySelector("input").value;
 
-  commentReplyBtn.addEventListener("click", () => {
-    commentInput.classList.add("comment__input-active");
-  });
+        if (text) {
+          const newCommentContainer = document.createElement("div");
+          newCommentContainer.classList.add("comment__container");
+
+          newCommentContainer.innerHTML = `
+          <div class="comment">
+            <h3 class="comment__text">${text}</h3>
+            <button type="button" class="comment__reply__btn">Reply</button>
+          </div>
+          <form class="comment__input">
+            <p>Comment:</p>
+            <div>
+              <input class="comment" type="text" />
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        `;
+
+          commentInput
+            .closest(".comment__container")
+            .appendChild(newCommentContainer);
+
+          commentInput.querySelector("input").value = "";
+          commentInput.classList.remove("comment__input-active");
+          handleComment();
+        }
+      });
+    });
+  };
+
+  handleComment();
 })();
